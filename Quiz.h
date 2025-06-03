b4a2c2c5a2bddce0849e8b85c131c251b0c2a2c4
HcommaMichael <michaelrh301@gmail.com>
2025-06-03 16:38:30 -0700

    Update Quiz.h

--- Quiz.h
+++ Quiz.h
@@ -1,1 +1,27 @@
+#ifndef QUIZ_H_
+#define QUIZ_H_
 
+#include "FlashcardLinkedNode.h"
+#include "Flashcard.h"
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+class Quiz {
+private:
+	FlashCardLinkedNode* copy;
+	int wrong;
+	int right;
+	std::vector<FlashCardLinkedNode*> nodes;
+public:
+	Quiz(FlashCardLinkedNode* deck);
+	void insertAndRandomize();
+	void startQuiz();
+	void nodeRandomizer(std::vector<FlashCardLinkedNode*> v);
+	void answerRandomizer(std::vector<std::string> a);
+};
+
+
+
+#endif
