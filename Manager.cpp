70637b6fd5ea4dd19f3dfc4088db126c8512e78f
Paul-Rip <paul_rip@yahoo.com>
2025-06-05 13:50:24 -0700

    Add files via upload

--- Manager.cpp
+++ Manager.cpp
@@ -1,1 +1,19 @@
+#include "Manager.h"
 
+Manager::Manager(flashCard* deck){
+    this->deck = deck;
+}
+void Manager::saveCards(std::ofstream file){
+    FlashCardLinkedNode* temp = deck->getDeck();
+    while(temp != nullptr){
+        file << "Q: " << temp->question;
+        file << "A: " << temp->answer;
+        temp = temp->next;
+    }
+}
+void Manager::loadCards(std::ifstream file){
+
+}
+flashCard* Manager::getDeck(){
+    return deck;
+}
--- Manager.h
+++ Manager.h
@@ -1,1 +1,18 @@
+#ifndef _MANAGER_H_
+#define _MANAGER_H_
 
+#include "FlashCard.h"
+#include <fstream>
+
+class Manager{
+    private:
+    flashCard* deck;
+    public:
+    Manager(flashCard* deck);
+    void saveCards(std::ofstream file);
+    void loadCards(std::ifstream file);
+    flashCard* getDeck();
+};
+
+
+#endif
