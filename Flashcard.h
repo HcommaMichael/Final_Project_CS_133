0c6c2c5502876bc3f0d61ca9c004250f91410d89
HcommaMichael <michaelrh301@gmail.com>
2025-06-08 17:51:05 -0700

    Update Flashcard.h

--- Flashcard.h
+++ Flashcard.h
@@ -4,17 +4,20 @@
 #include <string>
 #include "FlashcardLinkedNode.h"
 
-class flashCard{
-    private:
+class flashCard {
+private:
     FlashCardLinkedNode* deck = nullptr;
     std::string name;
     int size = 0;
-    public:
+public:
     flashCard(std::string topic);
     void addFlashcard(std::string q, std::string a);
+    FlashCardLinkedNode* addCardHelper(FlashCardLinkedNode* curr);
     void removeFlashcard(int index);
+    void removeFlashCardHelper(int index, int target, FlashCardLinkedNode* curr);
     void editFlashcard(int index);
     void printDeck();
+    void printDeckHelper(FlashCardLinkedNode* curr);
     int getSize();
     FlashCardLinkedNode* getDeck();
 };
