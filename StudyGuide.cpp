206e26d48cd7da2862b17a7f8e519d804d2005a1
HcommaMichael <michaelrh301@gmail.com>
2025-06-08 21:28:53 -0700

    Update StudyGuide.cpp

--- StudyGuide.cpp
+++ StudyGuide.cpp
@@ -3,8 +3,6 @@
 StudyGuide::StudyGuide(FlashCardLinkedNode* deck) {
 	FlashCardLinkedNode* curr = deck;
 	createCopy(curr);
-
-
 }
 void StudyGuide::createCopy(FlashCardLinkedNode* deck) {
 	if (deck == nullptr) {
@@ -27,7 +25,48 @@ void StudyGuide::constructorHelper() {
 	wrongDeck = wrongDeck->next;
 	front->next = nullptr;
 
+	answerPool.push_back(front->answer);
 	studyPriority.push(front);
 	constructorHelper();
-
 }
+void StudyGuide::startQuiz() {
+	std::srand(std::time(0));
+	std::vector<std::string> answers;
+	while (!studyPriority.empty()) {
+		answers.push_back(studyPriority.top()->answer);
+
+		while (answers.size() != 4) {
+			int index = std::rand() % answerPool.size();
+			if (!vContains(answerPool[index], 0, answers)) {
+				answers.push_back(answerPool[index]);
+			}
+		}
+		answerRandomizer(answers);
+		std::string choice;
+
+		std::cout << "Q: " << studyPriority.top()->question << std::endl;
+		std::cout << "1. " << answers[0] << std::endl << "2. " << answers[1] << std::endl
+			<< "3. " << answers[2] << std::endl << "4. " << answers[3] << std::endl;
+
+		std::getline(std::cin, choice);
+
+		if (answers[stoi(choice) - 1] == studyPriority.top()->answer) {
+			--studyPriority.top()->wrongAmount;
+			std::cout << "Correct!" << std::endl;
+		}
+		else {
+			std::cout << "Incorrect!" << std::endl << "The correct answer was: " <<
+				studyPriority.top()->answer << std::endl;
+		}
+		answers.clear();
+
+		if(studyPriority.top()->wrongAmount != 0){
+			studyPriority.push(studyPriority.top());
+			studyPriority.pop();
+		}
+		else {
+			studyPriority.pop();
+		}
+	}
+}
+
