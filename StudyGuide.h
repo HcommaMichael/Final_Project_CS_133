57729b308ff6c7692e3624907105752788442085
HcommaMichael <michaelrh301@gmail.com>
2025-06-08 19:04:23 -0700

    Update StudyGuide.h

--- StudyGuide.h
+++ StudyGuide.h
@@ -1,1 +1,19 @@
+#ifndef STUDY_GUIDE_H
+#define STUDY_GUIDE_H
+
+#include "FlashcardLinkedNode.h"
+#include <queue>
+
+class StudyGuide {
+private:
+	std::priority_queue<FlashCardLinkedNode*, std::vector<FlashCardLinkedNode*>, Comparator> studyPriority;
+	FlashCardLinkedNode* wrongDeck;
+public:
+	StudyGuide(FlashCardLinkedNode* deck);
+	void createCopy(FlashCardLinkedNode* deck);
+	void constructorHelper();
+};
+
+
+#endif
 
