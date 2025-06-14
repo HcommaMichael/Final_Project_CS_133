b222d5ed23d5b442a85c7d1e383848f76a946dae
HcommaMichael <michaelrh301@gmail.com>
2025-06-14 12:38:48 -0700

    Update StudyGuide.h
    comments

--- StudyGuide.h
+++ StudyGuide.h
@@ -7,9 +7,11 @@
 
 class StudyGuide: public Quiz{
 private:
-	std::priority_queue<FlashCardLinkedNode*, std::vector<FlashCardLinkedNode*>, Comparator> studyPriority;
-	FlashCardLinkedNode* wrongDeck;
-	std::vector<std::string> answerPool;
+	std::priority_queue<FlashCardLinkedNode*, std::vector<FlashCardLinkedNode*>, Comparator> studyPriority; // PQ used for max heap
+	FlashCardLinkedNode* wrongDeck; //consumable copy containing only wrong answered nodes
+	std::vector<std::string> answerPool; //vector holding all answers for entire deck, regardless of wrongAmount value
+
+	//public member functions unique to StudyGuide, commented in StudyGuide.cpp
 public:
 	StudyGuide(FlashCardLinkedNode* deck);
 	virtual void createCopy(FlashCardLinkedNode* deck);
