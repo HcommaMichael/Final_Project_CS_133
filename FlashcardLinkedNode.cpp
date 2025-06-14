74690b4d53d06799108bd1199d494bcb03dc7afa
HcommaMichael <michaelrh301@gmail.com>
2025-06-14 12:35:53 -0700

    Update FlashcardLinkedNode.cpp
    comments

--- FlashcardLinkedNode.cpp
+++ FlashcardLinkedNode.cpp
@@ -2,9 +2,10 @@
 #include <string>
 #include "FlashcardLinkedNode.h"
 
-
+//initialize a new node with a nullptr
 FlashCardLinkedNode::FlashCardLinkedNode(std::string question, std::string answer) : FlashCardLinkedNode(question, answer, nullptr) {}
 
+//initializes a new node with a non nullptr, used when adding to the front of a list mainly
 FlashCardLinkedNode::FlashCardLinkedNode(std::string question, std::string answer, FlashCardLinkedNode* next) {
     this->question = question;
     this->answer = answer;
