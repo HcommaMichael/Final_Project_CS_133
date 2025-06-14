42c6cb6a5b50f1b97665112a47b19a2ebe1c18be
HcommaMichael <michaelrh301@gmail.com>
2025-06-14 11:19:21 -0700

    Update StudyGuide.cpp

--- StudyGuide.cpp
+++ StudyGuide.cpp
@@ -1,70 +1,80 @@
 #include "StudyGuide.h"
 
 StudyGuide::StudyGuide(FlashCardLinkedNode* deck) {
-    FlashCardLinkedNode* curr = deck;
-    createCopy(curr);
-    constructorHelper();
+	FlashCardLinkedNode* curr = deck;
+	createCopy(curr);
+	constructorHelper();
 }
 void StudyGuide::createCopy(FlashCardLinkedNode* deck) {
-    if (deck == nullptr) {
-        return;
-    }
-    else if (deck->wrongAmount >= 1) {
-        answerPool.push_back(deck->answer);
-        wrongDeck = new FlashCardLinkedNode(deck->question, deck->answer, wrongDeck);
-        wrongDeck->wrongAmount = deck->wrongAmount;
-        createCopy(deck->next);
-    }
-    else {
-        answerPool.push_back(deck->answer);
-        createCopy(deck->next);
-    }
+	if (deck == nullptr) {
+		return;
+	}
+	else if (deck->wrongAmount >= 1) {
+		answerPool.push_back(deck->answer);
+		wrongDeck = new FlashCardLinkedNode(deck->question, deck->answer, wrongDeck);
+		wrongDeck->wrongAmount = deck->wrongAmount;
+		createCopy(deck->next);
+	}
+	else {
+		answerPool.push_back(deck->answer);
+		createCopy(deck->next);
+	}
 }
 void StudyGuide::constructorHelper() {
-    if (wrongDeck == nullptr) {
-        return;
-    }
-    FlashCardLinkedNode* front = wrongDeck;
-    wrongDeck = wrongDeck->next;
-    front->next = nullptr;
+	if (wrongDeck == nullptr) {
+		return;
+	}
+	FlashCardLinkedNode* front = wrongDeck;
+	wrongDeck = wrongDeck->next;
+	front->next = nullptr;
 
-    studyPriority.push(front);
-    constructorHelper();
+	studyPriority.push(front);
+	constructorHelper();
 }
 void StudyGuide::startQuiz() {
-    std::srand(std::time(0));
-    std::vector<std::string> answers;
-    while (!studyPriority.empty()) {
-        answers.push_back(studyPriority.top()->answer);
+	std::srand(std::time(0));
+	std::vector<std::string> answers;
+	while (!studyPriority.empty()) {
+		answers.push_back(studyPriority.top()->answer);
 
-        while (answers.size() != 4 && answers.size() < answerPool.size()) {
-            int index = std::rand() % answerPool.size();
-            if (!vContains(answerPool[index], 0, answers)) {
-                answers.push_back(answerPool[index]);
-            }
-        }
-        answerRandomizer(answers);
-        std::string choice;
-        std::cout << "Q: " << studyPriority.top()->question << std::endl;
-        std::cout << "1. " << answers[0] << std::endl << "2. " << answers[1] << std::endl
-            << "3. " << answers[2] << std::endl << "4. " << answers[3] << std::endl;
-        std::getline(std::cin, choice);
+		while (answers.size() != 4 && answers.size() < answerPool.size()) {
+			int index = std::rand() % answerPool.size();
+			if (!vContains(answerPool[index], 0, answers)) {
+				answers.push_back(answerPool[index]);
+			}
+		}
+		answerRandomizer(answers);
+		std::string choice;
+		std::cout << "Pick an answer (1-4) or q to quit" << std::endl;
+		std::cout << "Q: " << studyPriority.top()->question << std::endl;
+		std::cout << "------" << std::endl;
+		std::cout << "1. " << answers[0] << std::endl << "2. " << answers[1] << std::endl
+			<< "3. " << answers[2] << std::endl << "4. " << answers[3] << std::endl << std::endl;
+		std::cout << "Answer: ";
+		std::getline(std::cin, choice);
 
-        if (answers[stoi(choice) - 1] == studyPriority.top()->answer) {
-            --studyPriority.top()->wrongAmount;
-            std::cout << "Correct!" << std::endl;
-        }
-        else {
-            std::cout << "Incorrect!" << std::endl << "The correct answer was: " <<
-                studyPriority.top()->answer << std::endl;
-        }
-        answers.clear();
+		if (tolower(choice[0]) == 'q') {
+			std::cout << "Better luck next time!" << std::endl;
+			return;
+		}
 
-        FlashCardLinkedNode* top = studyPriority.top();
-        studyPriority.pop();
+		else if (answers[stoi(choice) - 1] == studyPriority.top()->answer) {
+			--studyPriority.top()->wrongAmount;
+			std::cout << "Correct!" << std::endl;
+		}
+		else {
+			std::cout << "Incorrect!" << std::endl << "The correct answer was: " <<
+				studyPriority.top()->answer << std::endl;
+		}
+		answers.clear();
 
-        if (top->wrongAmount != 0) {
-            studyPriority.push(top);
-        }
-    }
+		FlashCardLinkedNode* top = studyPriority.top();
+		studyPriority.pop();
+
+		if (top->wrongAmount != 0) {
+			studyPriority.push(top);
+		}
+	}
+	std::cout << "You got them all!" << std::endl;
 }
+
