425ecd1517ef2b4160c2a358ab320c35318cbc9b
HcommaMichael <michaelrh301@gmail.com>
2025-06-14 11:18:47 -0700

    Update Manager.cpp

--- Manager.cpp
+++ Manager.cpp
@@ -1,46 +1,46 @@
-
 #include "Manager.h"
 #include <iostream>
 
-Manager::Manager(FlashCardLinkedNode* deck, std::string name){
+Manager::Manager(FlashCardLinkedNode* deck, std::string name) {
     this->deck = deck;
     this->name = name;
 }
-void Manager::saveCards(std::ofstream& file){
+void Manager::saveCards(std::ofstream& file) {
     FlashCardLinkedNode* temp = deck;
-    if(!file){
+    if (!file) {
         throw "invalid file";
     }
-    while(temp != nullptr){
+    while (temp != nullptr) {
         file << "Q: " << temp->question;
         file << "A: " << temp->answer << "\n";
         temp = temp->next;
     }
     file.close();
 }
-void Manager::loadCards(std::ifstream& file){
+void Manager::loadCards(std::ifstream& file) {
     std::string line;
     std::string sub;
     std::string sub2;
     int x;
     int y;
     FlashCardLinkedNode* temp = deck;
-    if(!file){
+    if (!file) {
         throw "invalid file";
     }
-    while(getline(file, line)){
-        if(deck == nullptr){
+    while (getline(file, line)) {
+        if (deck == nullptr) {
             x = line.find("Q: ");
             y = line.find("A: ");
-            sub = line.substr(x + 3, y-(x+3));
-            sub2 = line.substr(y+3);
+            sub = line.substr(x + 3, y - (x + 3));
+            sub2 = line.substr(y + 3);
             deck = new FlashCardLinkedNode(sub, sub2);
             temp = deck;
-        }else{
+        }
+        else {
             x = line.find("Q: ");
             y = line.find("A: ");
-            sub = line.substr(x + 3, y-(x+3));
-            sub2 = line.substr(y+3);
+            sub = line.substr(x + 3, y - (x + 3));
+            sub2 = line.substr(y + 3);
             temp->next = new FlashCardLinkedNode(sub, sub2);
             temp = temp->next;
         }
@@ -48,14 +48,14 @@ void Manager::loadCards(std::ifstream& file){
     }
     file.close();
 }
-FlashCardLinkedNode* Manager::getDeck(){
+FlashCardLinkedNode* Manager::getDeck() {
     return deck;
 }
 
-std::string Manager::getName(){
+std::string Manager::getName() {
     return name;
 }
 
-int Manager::getSize(){
+int Manager::getSize() {
     return size;
 }
