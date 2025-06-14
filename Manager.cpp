f55f54f2bd82423ddf1cc48e3d6662ebea888824
Paul-Rip <paul_rip@yahoo.com>
2025-06-14 12:08:49 -0700

    Update Manager.cpp

--- Manager.cpp
+++ Manager.cpp
@@ -1,21 +1,21 @@
 #include "Manager.h"
 #include <iostream>
 
-Manager::Manager(FlashCardLinkedNode* deck, std::string name) {
+Manager::Manager(FlashCardLinkedNode* deck, std::string name) { //constructor that assigns the deck and name of the deck
     this->deck = deck;
     this->name = name;
 }
 void Manager::saveCards(std::ofstream& file) {
     FlashCardLinkedNode* temp = deck;
-    if (!file) {
+    if (!file) { //if the file can not be opened, throws a string exception below
         throw "invalid file";
     }
-    while (temp != nullptr) {
+    while (temp != nullptr) { //while there are still cards then it will output to the file in the format of Q: questionA: answer
         file << "Q: " << temp->question;
         file << "A: " << temp->answer << "\n";
         temp = temp->next;
     }
-    file.close();
+    file.close(); //closes the file
 }
 void Manager::loadCards(std::ifstream& file) {
     std::string line;
@@ -24,27 +24,27 @@ void Manager::loadCards(std::ifstream& file) {
     int x;
     int y;
     FlashCardLinkedNode* temp = deck;
-    if (!file) {
+    if (!file) { //if the file can not be opened, throws a string exception below
         throw "invalid file";
     }
-    while (getline(file, line)) {
-        if (deck == nullptr) {
+    while (getline(file, line)) { //while there are cards in the file, it will create new cards with the question and answers inside the file 
+        if (deck == nullptr) { //assigns the first card
             x = line.find("Q: ");
             y = line.find("A: ");
             sub = line.substr(x + 3, y - (x + 3));
             sub2 = line.substr(y + 3);
-            deck = new FlashCardLinkedNode(sub, sub2);
+            deck = new FlashCardLinkedNode(sub, sub2); //takes the question and answer without the Q: and A: and puts it into the deck
             temp = deck;
         }
-        else {
+        else { //assigns the rest of the cards
             x = line.find("Q: ");
             y = line.find("A: ");
             sub = line.substr(x + 3, y - (x + 3));
             sub2 = line.substr(y + 3);
-            temp->next = new FlashCardLinkedNode(sub, sub2);
+            temp->next = new FlashCardLinkedNode(sub, sub2); //takes the question and answer without the Q: and A: and puts it into the deck
             temp = temp->next;
         }
-        size++;
+        size++; //increments the size so the deck size is accurate
     }
     file.close();
 }
